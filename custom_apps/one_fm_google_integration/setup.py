f3769f80d5cb85cb4f8971d076ea3e4a19d13cd7
SK Rohit <skr434546@gmail.com>
2025-10-26 00:47:22 +0530

    Changed the Project structure

---
+++ custom_apps/one_fm_google_integration/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='one_fm_google_integration', # Replace if your module name is different
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[], # Add dependencies here if needed, e.g., ['requests']
+)
